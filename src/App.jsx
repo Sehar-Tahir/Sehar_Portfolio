ec0bb7655187229f3e962931e1456f488ab5caf5
Sehar-Tahir <sehartahirofficial@gmail.com>
2025-03-26 20:15:16 +0500

    Projects Updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Home />
       <About />
       <Skills />
-      <Experience/>
+      <Experience />
       <Projects />
       <Footer />
     </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,10 @@
 import ProjectCard from './ProjectCard'
-import Project1 from '../../assets/Project1.png'
 import Project2 from '../../assets/Project2.png'
 import Project3 from '../../assets/Project3.png'
-import Project4 from '../../assets/Project4.png'
 import Project5 from '../../assets/Project5.png'
 import Project6 from '../../assets/Project6.png'
+import Project7 from '../../assets/Project7.png'
+import Portfolio from '../../assets/Portfolio.png'
 
 const Projects = () => {
   return (
@@ -19,15 +19,13 @@ const Projects = () => {
         <ProjectCard title='Tailwind Project' imgsrc={Project3} tec='Html, CSS, Tailwind Css' desc='A stunning and Responsive Landing Page 
         Dev.Learnings created by Html, CSS and Tailwind Css. Responsive landing page design across all devices.' git='https://github.com/Sehar-Tahir/Responsive_Landing_Page-TailwindCss' demo='https://responsive-landing-page-tailwind-css.vercel.app/' />
 
-        <ProjectCard title='Task Tracker' imgsrc={Project4} tec='React Js' desc='Task Tracker is web app to create and manage tasks & also add reminder for important tasks.React hooks are used to build it.' git='https://github.com/Sehar-Tahir/Task_Tracker' demo='https://task-tracker-ebon-eight.vercel.app/' />
+        <ProjectCard title='Portfolio' imgsrc={Portfolio} tec='React Js, Tailwind Css, Email.js' desc='This is my portfolio Project, build with React and Tailwind CSS. Email.js is used for Email Functionality.' git='https://github.com/Sehar-Tahir/Sehar_Portfolio' demo='https://sehar-portfolio.vercel.app/' />
 
         <ProjectCard title='Web Dev Site' imgsrc={Project5} tec='React Js, Material UI' desc='
-                        Web Dev Site is a community platform where web developers connect, share, 
-                        and learn from each other. Built with Material UI components.' git='https://github.com/Sehar-Tahir/Web_Dev_Site' demo='https://web-dev-site-ten.vercel.app/' />
+        Web Dev Site is a community platform where web developers connect, share, 
+        and learn from each other. Built with Material UI components.' git='https://github.com/Sehar-Tahir/Web_Dev_Site' demo='https://web-dev-site-ten.vercel.app/' />
 
-
-        <ProjectCard title='Promptify' imgsrc={Project1} tec='Next Js, Tailwind [Under Working]' desc='Promtify is a AI-powered platform to generate innovative ideas and inspiration, Discover, share, and collaborate
-                on prompts with Promptify!' git='' demo='' />
+        <ProjectCard title='UniGradeBook' imgsrc={Project7} tec='React Js, Tailwind Css' desc='UniGradeBook is a website created for students to select their favourite professor for studies. and they can also see the results of their previous students.' git='https://github.com/Sehar-Tahir/UniGradebook' demo='https://uni-gradebook.vercel.app/' />
 
         <ProjectCard title='Bootstrap Project' imgsrc={Project6} tec='Html, CSS, Bootstrap' desc='Dev Learnings is a  responsive landing page created by html,
          CSS and bootstrap components are used to built this landing page.' git='https://github.com/Sehar-Tahir/Responsive_Landing_page' demo='https://responsive-landing-page-alpha.vercel.app/' />
